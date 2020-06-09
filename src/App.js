952fbeede55aff2fa59f2b7c08ba76ba419f1f16
jason-nordheim <jason.nordheim@gmail.com>
2020-06-09 13:45:04 -0600

    fixed re-render

--- src/App.js
+++ src/App.js
@@ -1,68 +1,36 @@
 import React, {useState, useEffect} from 'react';
 import PetitionContainer from './components/PetitionsContainer'
 
+
 function App() {
   const TITLE = `Rise above`
   const BASE_URL = `http://localhost:3000/`
 
-  // const [signees, setSignees] = useState([])
   const [petitions, setPetitions] = useState([])
   const [loaded, setLoaded] = useState(false)
 
-  // const getSignees = () => {
-  //   fetch(BASE_URL + "signees")
-  //     .then(response => response.json())
-  //     .then(data => saveSignees(data))
-  // }
-
-  // const saveSignees = (data) => {
-  //   const parsedSignees = data.map(s => {
-  //     return {
-  //       first_name: s.first_name, 
-  //       last_name: s.last_name, 
-  //       address: s.address
-  //     }
-  //   })
-  //   setSignees(parsedSignees)
-  //   setLoaded(true)
-  // }
-  
-  // const savePetitions = (data) => {
-  //   console.log('data', data)
-  //   const parsedPetitions = data.map(p => {
-  //     const pet = {
-  //       id: p.id, 
-  //       description: p.description, 
-  //       name: p.name, 
-  //       signature_goal: p.signature_goal, 
-  //       submit_to: p.submit_to
-  //     }
-  //     return pet 
-  //   })
-  //   setPetitions(parsedPetitions)
-  // }
-
-  const getPetitions = () => {
+  function getPetitions(){
     fetch(BASE_URL + 'petitions')
-        .then(response => response.json())
-        .then(data => setPetitions(data))
-        .then(result => setLoaded(true))
+    .then(response => response.json())
+    .then(data => {
+      console.log('data', data)
+      setPetitions(data)
+      setLoaded(true)
+      return data})
   }
-
-  useEffect(() => {
-    getPetitions() 
-  },[])
-
+  
+  useEffect(getPetitions,[])
 
   const sendProps = {app_url: BASE_URL, petitions}
 
+  console.log('render')
   return (
     <div className="App">
       <header>
         <h1>{TITLE}</h1>
       </header>
       <main>
-        { loaded ? <PetitionContainer {...sendProps} /> : null}
+      { loaded ? <PetitionContainer {...sendProps} /> : "no content"}
       </main>
     </div>
   );
--- src/components/PetitionsContainer.js
+++ src/components/PetitionsContainer.js
@@ -4,24 +4,20 @@ import PetitionCard from './PetitionCard'
 
 export default function PetitionsContainer(props) {
     const [petitionElements, setPetitionElements] = useState([])
-    const {petitions } = props
+    const {app_url, petitions} = props
     
+    console.log(petitions)
+
     useEffect(() => {
         if (petitions && petitions.length > 0) {
-            const petitionsElements = petitions.map(p => {
-                return ( <PetitionCard key={p.id} {...p} /> )
-            })
+            const petitionsElements = petitions.map(p => ( 
+                <PetitionCard key={p.id} {...p} /> ))
             setPetitionElements(petitionsElements)
         }   
-    },[])
-
-    const styles = {
-        display: "flex",
-        backgroundColor: "black"
-    }
+    }, [])
 
     return (
-        <div styles={styles} className="petition-container">
+        <div className="petition-container">
             <div>
                 {petitionElements}
             </div>
